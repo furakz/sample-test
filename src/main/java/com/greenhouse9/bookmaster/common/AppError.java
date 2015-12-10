c08649a33e7bd4bdfbaf91a1b5249fc920068844
funa taro <@greenhouse9.com>
2015-12-11 07:51:59 +0900

    check

---
+++ src/main/java/com/greenhouse9/bookmaster/common/AppError.java
@@ -0,0 +1,15 @@
+package com.greenhouse9.bookmaster.common;
+
+public class AppError {
+
+	private String errorMessage;
+
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+
+	public void setErrorMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
+
+}
--- src/main/java/com/greenhouse9/bookmaster/domain/BookHelper.java
+++ src/main/java/com/greenhouse9/bookmaster/domain/BookHelper.java
@@ -1,6 +1,97 @@
 package com.greenhouse9.bookmaster.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.greenhouse9.bookmaster.common.AppError;
+
 public class BookHelper {
+
+	private List<AppError> errorList = new ArrayList<AppError>();
+
+	public void bind(BookInput bookInput, Book book) {
+
+		if (book == null){
+			book = new Book();
+		}
+
+		boolean isValid = true;
+		AppError err;
+
+		//id
+		Long id = null;
+		if (isEmpty(bookInput.getId())) {
+			isValid = false;
+			err = new AppError();
+			err.setErrorMessage("id is empty");
+			errorList.add(err);
+		}
+		if(isValid){
+
+			try {
+				id = Long.parseLong(bookInput.getId());
+			} catch(Exception e) {
+				isValid = false;
+				err = new AppError();
+				err.setErrorMessage("id is not number format");
+				errorList.add(err);
+			}
+		}
+		if (isValid){
+			book.setId(id);
+		}
+
+		//title
+		isValid = true;
+		String title = "";
+		if (isEmpty(bookInput.getTitle())) {
+			isValid = false;
+			err = new AppError();
+			err.setErrorMessage("title is empty");
+			errorList.add(err);
+		}
+		if(isValid){
+
+			title = bookInput.getTitle();
+			if (title.length() > 20) {
+				isValid = false;
+				err = new AppError();
+				err.setErrorMessage("title is too long (max 20)");
+				errorList.add(err);
+			}
+		}
+		if(isValid){
+			book.setTitle(title);
+		}
+
+		//price prec 9, scale 2
+		isValid = true;
+		Float price = null;
+		if (!isEmpty(bookInput.getPrice())) {
+			if (!bookInput.getPrice().matches("^\\d{1,7}([\\.]\\d{1,2})?$")){
+				isValid = false;
+				err = new AppError();
+				err.setErrorMessage("price is not float");
+				errorList.add(err);
+			};
+			if(isValid){
+
+				try {
+					price = Float.parseFloat(bookInput.getPrice());
+				} catch (Exception e) {
+					isValid = false;
+					err = new AppError();
+					err.setErrorMessage("price is not float");
+					errorList.add(err);
+				}
+			}
+		}
+		if(isValid){
+			book.setPrice(price);
+		}
+
+
+}
 
 	public Book getBook(BookInput bookInput) {
 
@@ -40,6 +131,14 @@ public class BookHelper {
 			e.printStackTrace();
 			throw e;
 		}
+	}
+
+	public Boolean hasErrors(){
+		return !errorList.isEmpty();
+	}
+
+	public Boolean noErrors(){
+		return errorList.isEmpty();
 	}
 
 	private boolean isEmpty(String str){
